26d7f391b3a3febc915875392ba90f410501b42b
KageHayashi <kagehayashi@gmail.com>
2021-12-16 14:32:20 -0800

    Added temp background images. Added button to go to kitchen scene in bakery scene.

--- MainMenu.gd
+++ MainMenu.gd
@@ -1,24 +1,16 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$"VBoxContainer/Start Button".grab_focus()
 
 
 func _on_Start_Button_pressed():
-	get_tree().change_scene("res://ToastyBoi.tscn")
+	get_tree().change_scene("res://InsideBackeryScene.tscn")
 
 
 #func _on_Options_Button_pressed():
 #	get_tree().change_scene("res://MainScene.tscn")
-
-
 
 
 func _on_Exit_Button_pressed():
