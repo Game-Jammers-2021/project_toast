705bebd6ab49558b6a295a904b0e7d0c9894496c
KageHayashi <kagehayashi@gmail.com>
2021-12-17 15:46:54 -0800

    Fixed bug where only one toast is displayed.

--- Scenes/InsideBackeryScene.gd
+++ Scenes/InsideBackeryScene.gd
@@ -1,20 +1,21 @@
 extends Node2D
 
+var toasts_on_counter = Global.toasts_on_counter
+
 # When entering scene, check for all toasts that should
 # be on counter and show them
 func _ready():
-	var toasts_on_counter = Global.toasts_on_counter
 	for i in get_node("ToastsOnCounter").get_children():
 		i.visible = false
 	if toasts_on_counter.has("EggAv"):
 		get_node("ToastsOnCounter/EggAvToast").visible = true
-	elif toasts_on_counter.has("EggJam"):
+	if toasts_on_counter.has("EggJam"):
 		get_node("ToastsOnCounter/EggJamToast").visible = true
-	elif toasts_on_counter.has("EggButter"):
+	if toasts_on_counter.has("EggButter"):
 		get_node("ToastsOnCounter/EggButterToast").visible = true
-	elif toasts_on_counter.has("AvButter"):
+	if toasts_on_counter.has("AvButter"):
 		get_node("ToastsOnCounter/AvButterToast").visible = true
-	elif toasts_on_counter.has("AvJam"):
+	if toasts_on_counter.has("AvJam"):
 		get_node("ToastsOnCounter/AvJamToast").visible = true
-	elif toasts_on_counter.has("JamButter"):
+	if toasts_on_counter.has("JamButter"):
 		get_node("ToastsOnCounter/JamButterToast").visible = true
